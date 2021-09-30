3920a082122ab808e1865eada6ce015eb19be0bf
Arod72564 <aerodriguez3@cpp.edu>
2021-09-29 21:28:41 -0700

    Added restaurantBill.c and guessingGame.c; wrote outline for code to be added for each program.

---
+++ guessingGame.c
@@ -0,0 +1,26 @@
+//CS2600 Number Guessing Game
+
+#include <stdio.h>
+
+int main() {
+
+    // Display menu options 1-3 
+
+    // Prompt user to enter a number for option 1-3
+
+    // If user selects option 1
+    // Randomly generate number
+    // Prompt user to guess a number
+    // if the user guesses correctly display win prompt and return to menu.
+    // if incorrect tell the user if they are lower or higher than the number and prompt them to guess again.
+    // if user enters 'q' return to menu
+
+    // If user selects option 2
+    // Display max value user can set number to.
+    // Prompt user to enter a number greater than 0 and less than max
+
+    // If user selects option 3
+    // Display exit prompt and quit program
+
+    return 0;
+}
---
+++ restaurantBill.c
@@ -0,0 +1,24 @@
+/*  CS2600 Restaurant Bill
+
+Write a program that computes the tax and tip on a bill.
+Program shold accept the tax and tip bot has percentages
+from the command line. Display the meal cost, tax amount,
+and total bill on screen. */
+
+#include <stdio.h>
+ 
+int main() {
+    
+    // Display menu items and prices
+
+    // Prompt user for tax amount and convert to percentage
+
+    // Prompt user for tip amount and convert to percentage
+
+    // Randomly choose between menu item 1-4
+
+    // Calculate meal cost, tax amount, tip amount, 
+    // and total then display
+
+    return 0;
+}
